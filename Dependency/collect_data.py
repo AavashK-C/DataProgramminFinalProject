0ecd599b2e45300dc68d949bd3837f925003fbaa
AavashK-C <148005117+AavashK-C@users.noreply.github.com>
2023-12-13 14:09:32 -0500

    Update collect_data.py

--- Dependency/collect_data.py
+++ Dependency/collect_data.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from forex_python.converter import CurrencyRates
-from datetime import datetime, timedelta
+from datetime import datetime
 
 # Connect to the MongoDB database
 client = MongoClient("mongodb+srv://Aavash:Aavash123@cluster0.8ejpscx.mongodb.net/?retryWrites=true&w=majority")
@@ -13,15 +13,14 @@ c = CurrencyRates()
 # Define the currency pairs
 currency_pairs = [("USD", "EUR"), ("USD", "GBP"), ("USD", "CAD")]  # Example currency pairs
 
-# Fetch currency rates for the previous 30 days and save them in MongoDB
-for i in range(30):
-    target_date = datetime.now().date() - timedelta(days=i + 1)  # Get the date for the previous day
-    for pair in currency_pairs:
-        rate = c.get_rate(pair[0], pair[1], target_date)
-        data = {
-            "Date": target_date.isoformat(),
-            "FromCurrency": pair[0],
-            "ToCurrency": pair[1],
-            "ExchangeRate": rate
-        }
-        collection.insert_one(data)
+# Fetch currency rates and save them in MongoDB with today's date
+for pair in currency_pairs:
+    rate = c.get_rate(pair[0], pair[1])
+    today_date = datetime.now().date().isoformat()
+    data = {
+        "Date": today_date,
+        "FromCurrency": pair[0],
+        "ToCurrency": pair[1],
+        "ExchangeRate": rate
+    }
+    collection.insert_one(data)
